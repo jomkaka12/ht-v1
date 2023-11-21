40f8fb53ad58114a0d4ca40edbe3e0b465e1f2d1
jomkaka12 <112831588+jomkaka12@users.noreply.github.com>
2023-11-21 16:23:57 -0300

    Update HTbolado.lua

--- HTbolado.lua
+++ HTbolado.lua
@@ -65,31 +65,11 @@ Tab:AddButton({
 Tab:AddButton({
     Name = "Server Hop",
     Callback = function()
-        local Player = game.Players.LocalPlayer    
-local Http = game:GetService("HttpService")
-local TPS = game:GetService("TeleportService")
-local Api = "https://games.roblox.com/v1/games/"
-
-local _place,_id = game.PlaceId, game.JobId
-local _servers = Api.._place.."/servers/Public?sortOrder=Desc&limit=100"
-function ListServers(cursor)
-   local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
-   return Http:JSONDecode(Raw)
-end
-
-local Next; repeat
-   local Servers = ListServers(Next)
-   for i,v in next, Servers.data do
-       if v.playing < v.maxPlayers and v.id ~= _id then
-           local s,r = pcall(TPS.TeleportToPlaceInstance,TPS,_place,v.id,Player)
-           if s then break end
-       end
-   end
-   
-   Next = Servers.nextPageCursor
-until not Next
-    end
+        loadstring(game:HttpGet("https://www.scriptblox.com/raw/Server-Browser_80", true))();
+        end
+    })
 })
+
 
 local Tab = Window:MakeTab({
     Name = "Lixeiras",
